a8c27d4965e5e5974b761169e5684e5f1155474f
Aleksandr Tsarrytsanskyi <carekjustin2009@gmail.com>
2025-07-30 19:01:24 +0300

    init

--- PsyStrah/public/javascripts/main.js
+++ PsyStrah/public/javascripts/main.js
@@ -42,4 +42,35 @@ document.addEventListener("DOMContentLoaded", function() {
     //     })
     // })
 
+
+
+    function startCountdown(endDate, elementId) {
+  const countdownElement = document.getElementById("date_timer");
+
+  function updateCountdown() {
+    const now = new Date().getTime();
+    const timeLeft = endDate - now;
+
+    if (timeLeft < 0) {
+      countdownElement.innerHTML = "Акция завершена!";
+      clearInterval(countdownInterval);
+      return;
+    }
+
+    
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    countdownElement.innerHTML = `До конца акции:  ${hours}ч ${minutes}м ${seconds}с`;
+  }
+
+  updateCountdown(); // Обновляем сразу при загрузке страницы
+  const countdownInterval = setInterval(updateCountdown, 1000);
+}
+
+// Пример использования:
+const акцииEndDate = new Date("2030-07-31T23:59:59").getTime(); // Замените на реальную дату окончания акции
+startCountdown(акцииEndDate, "date_timer"); // id элемента для отображения таймера
+
 })
