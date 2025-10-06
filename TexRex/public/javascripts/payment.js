5d7bfe001c99aa11ea18bab9e1ae4af21e80d7fd
Aleksandr Tsarrytsanskyi <carekjustin2009@gmail.com>
2025-10-06 09:46:13 +0300

    init

---
+++ TexRex/public/javascripts/payment.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+//    const radioSpan = document.querySelectorAll(".payment .save_radio") 
+//    const labelSaveAdres = document.querySelectorAll(".payment .radio_span") 
+//    const save = document.querySelectorAll(".payment .save") 
+//    radioSpan.forEach(radio => {
+//     radio.addEventListener("change", function(event) {
+      
+//         if(event.target.checked) {
+//          save.forEach(el => {
+//             labelSaveAdres.forEach(span => {
+
+//                console.log(el.before());
+               
+//             })
+//          })
+           
+            
+//         }
+//         else {
+
+//         }
+//     })
+//    })
+})
