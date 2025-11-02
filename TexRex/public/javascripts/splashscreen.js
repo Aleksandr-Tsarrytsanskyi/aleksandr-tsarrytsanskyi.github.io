991960fc2175974fe8fb5f6695503592677ff4fa
Aleksandr Tsarrytsanskyi <carekjustin2009@gmail.com>
2025-11-02 14:05:42 +0200

    Update splashscreen.js

--- TexRex/public/javascripts/splashscreen.js
+++ TexRex/public/javascripts/splashscreen.js
@@ -4,25 +4,25 @@ document.addEventListener("DOMContentLoaded", function() {
     
        
 
-      //   function splashScreen() {
-      //       const splashScreen = document.querySelector('.splash');
-      //   const content = document.querySelector('.container');
+        function splashScreen() {
+            const splashScreen = document.querySelector('.splash');
+        const content = document.querySelector('.container');
 
-      // // Анимация для скрытия splash screen
-      //  setTimeout(() => {
-      //    splashScreen.classList.add("splashScreen_show")
-      //  }, 1000)
+      // Анимация для скрытия splash screen
+       setTimeout(() => {
+         splashScreen.classList.add("splashScreen_show")
+       }, 1000)
     
 
-      // // Показываем контент после завершения анимации
-      // splashScreen.addEventListener('transitionend', function() {
-      //   setTimeout(() => {
-      //     splashScreen.remove(); // Удаляем splash screen
-      //   }, 1000)
-      //   content.style.display = 'block'; // Показываем контент
-      // }, { once: true });
-      //   }
+      // Показываем контент после завершения анимации
+      splashScreen.addEventListener('transitionend', function() {
+        setTimeout(() => {
+          splashScreen.remove(); // Удаляем splash screen
+        }, 1000)
+        content.style.display = 'block'; // Показываем контент
+      }, { once: true });
+        }
 
-      //   splashScreen();
+        splashScreen();
     
 })
