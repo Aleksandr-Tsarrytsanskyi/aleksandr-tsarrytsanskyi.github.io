c9e7168bf864a63af57e5a82d6c6ec32b216d75f
Aleksandr Tsarrytsanskyi <carekjustin2009@gmail.com>
2025-11-06 10:15:36 +0200

    init

--- TexRex/public/javascripts/splashscreen.js
+++ TexRex/public/javascripts/splashscreen.js
@@ -9,16 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
         const content = document.querySelector('.container');
 
       // Анимация для скрытия splash screen
-       setTimeout(() => {
+      
          splashScreen.classList.add("splashScreen_show")
-       }, 1000)
+       
     
 
       // Показываем контент после завершения анимации
       splashScreen.addEventListener('transitionend', function() {
-        setTimeout(() => {
+     
           splashScreen.remove(); // Удаляем splash screen
-        }, 1000)
+        
         content.style.display = 'block'; // Показываем контент
       }, { once: true });
         }
