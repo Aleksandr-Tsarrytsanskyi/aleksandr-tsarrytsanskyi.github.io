8a5d59d15ceeb881a0f4078bf6302ea1b0ea69a3
Aleksandr Tsarrytsanskyi <carekjustin2009@gmail.com>
2025-11-08 10:31:48 +0200

    init

--- TexRex/public/javascripts/favoritesClear.js
+++ TexRex/public/javascripts/favoritesClear.js
@@ -1,4 +1,5 @@
  const removeProducts = document.querySelector(".favorites_filtr button");
+ const products = document.querySelectorAll(".products > .cell");
  removeProducts.addEventListener("click", function() {
    products.forEach(ptoduct => {
     ptoduct.remove()
