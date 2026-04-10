42511fbbd514ae26b4cf4c21afb69425c24429e9
alexmysite <73168503+Aleksandr-Tsarrytsanskyi@users.noreply.github.com>
2026-04-10 19:17:46 +0300

    init

---
+++ projectReact/src/components/header/header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return(
+      <header>
+        <h1 className="text-9xl font-roboto">My project</h1>
+      </header>
+        
+    )
+}
---
+++ projectReact/src/components/navbar/link.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const link = (props: Props) => {
+  return (
+    <div>link</div>
+  )
+}
+
+export default link
