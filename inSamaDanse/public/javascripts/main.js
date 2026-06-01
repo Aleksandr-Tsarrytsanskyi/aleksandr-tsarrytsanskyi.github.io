031097e784f381b879b9851f11d25ed5f2237d72
alexmysite <73168503+Aleksandr-Tsarrytsanskyi@users.noreply.github.com>
2026-06-01 13:29:02 +0300

    init

--- inSamaDanse/public/javascripts/main.js
+++ inSamaDanse/public/javascripts/main.js
@@ -1,118 +1,196 @@
 // import '../stylesheets/style.css';
 
 function init() {
-    function changeAcardion() {
-  const acardion = document.querySelectorAll(".acardion");
-  console.log(acardion);
 
-  const contentActive = document.querySelector(".acardion_content_active");
-  contentActive.style.maxHeight = contentActive.scrollHeight + "px";
-  acardion.forEach((el) => {
-    el.addEventListener("click", function (e) {
-      console.log(e.target);
-      acardion.forEach((btn) => {
-        btn.classList.remove("acardion_show");
-      });
-
-      let content = el.nextElementSibling;
-      let bgAcardion = el.parentElement
-
-      console.dir(bgAcardion);
-document.querySelectorAll(".bg_active_acardion").forEach((el) => {
-          el.classList.remove("acardion_bg_active")
-        });
-          bgAcardion.classList.add("acardion_bg_active")
+   function modalController({modal, btnOpen, btnClose, time = 300}) {
+  const buttonElements = document.querySelectorAll(btnOpen);
+  const modalElem = document.querySelector(modal);
+  // const blurmain = document.querySelector("main")
+  // const blurfooter = document.querySelector("footer")
+  // const blurheadr = document.querySelector("header")
+    
+  const openMOdal = () => {
+    modalElem.classList.add("active")
+    document.body.style.overflow = 'hidden';
+    
+    window.addEventListener("keydown", closeModal)
      
-
-      if (content.style.maxHeight) {
-        console.dir("11111");
- bgAcardion.classList.remove("acardion_bg_active")
-        document.querySelectorAll(".acardion__content").forEach((el) => {
-          el.style.maxHeight = null;
-        });
-
-        
-      } else {
-        console.log("22222");
-
-        document.querySelectorAll(".acardion__content").forEach((els) => {
-          els.style.maxHeight = null;
-          el.classList.add("acardion_show");
-          
-          content.style.maxHeight = content.scrollHeight + "px";
-        });
-      }
-    });
+    
+  };
+  buttonElements.forEach((item) => {
+    item.addEventListener("click", openMOdal);
   });
+  modalElem.addEventListener("click", closeModal);
+
+  function closeModal(event) {
+    const target = event.target;
+
+    if (target == modalElem || (btnClose && target.closest(btnClose)) || event.code === "Escape") {
+      modalElem.classList.remove("active");
+      document.body.style.overflow = 'visible'; 
+    
+      
+      window.removeEventListener("keydown", closeModal)
+    }
+  }
+    
 }
 
-function swiper() {
+modalController({
+  modal: ".burger-dialog",
+  btnOpen: ".burger-btn",
+  btnClose: ".close-btn",
+  time: "300",
+});
+  function changeAcardion() {
+    const acardion = document.querySelectorAll(".acardion");
+    console.log(acardion);
+
+    const contentActive = document.querySelector(".acardion_content_active");
+    contentActive.style.maxHeight = contentActive.scrollHeight + "px";
+    acardion.forEach((el) => {
+      el.addEventListener("click", function (e) {
+        console.log(e.target);
+        acardion.forEach((btn) => {
+          btn.classList.remove("acardion_show");
+        });
+
+        let content = el.nextElementSibling;
+        let bgAcardion = el.parentElement;
+
+        console.dir(bgAcardion);
+        document.querySelectorAll(".faq-course-content .bg_active_acardion").forEach((el) => {
+          el.classList.remove("acardion_bg_active");
+        });
+        bgAcardion.classList.add("acardion_bg_active");
+
+        if (content.style.maxHeight) {
+          console.dir("11111");
+          bgAcardion.classList.remove("acardion_bg_active");
+          document.querySelectorAll(".faq-course-content .acardion__content").forEach((el) => {
+            el.style.maxHeight = null;
+          });
+        } else {
+          console.log("22222");
+
+          document.querySelectorAll(".faq-course-content .acardion__content").forEach((els) => {
+            els.style.maxHeight = null;
+            el.classList.add("acardion_show");
+
+            content.style.maxHeight = content.scrollHeight + "px";
+          });
+        }
+      });
+    });
+  }
+
+  function changeAcardionFaq() {
+    const acardion = document.querySelectorAll(".acardion-faq");
+    console.log(acardion);
+
+    const contentActive = document.querySelector(".acardion_content_active-faq");
+    contentActive.style.maxHeight = contentActive.scrollHeight + "px";
+    acardion.forEach((el) => {
+      el.addEventListener("click", function (e) {
+        console.log(e.target);
+        acardion.forEach((btn) => {
+          btn.classList.remove("acardion_show");
+        });
+
+        let content = el.nextElementSibling;
+        let bgAcardion = el.parentElement;
+
+        console.dir(bgAcardion);
+        document.querySelectorAll(".faq-content .bg_active_acardion").forEach((el) => {
+          el.classList.remove("acardion_bg_active");
+        });
+        bgAcardion.classList.add("acardion_bg_active");
+
+        if (content.style.maxHeight) {
+          console.dir("11111");
+          bgAcardion.classList.remove("acardion_bg_active");
+          document.querySelectorAll(".faq-content .acardion__content-faq").forEach((el) => {
+            el.style.maxHeight = null;
+          });
+        } else {
+          console.log("22222");
+
+          document.querySelectorAll(".faq-content .acardion__content-faq").forEach((els) => {
+            els.style.maxHeight = null;
+            el.classList.add("acardion_show");
+
+            content.style.maxHeight = content.scrollHeight + "px";
+          });
+        }
+      });
+    });
+  }
+
+  function swiper() {
     const swiperPrev = document.querySelector(".swiperPrev");
-  const swiperNext = document.querySelector(".swiperNext");
-   const reviews = new Swiper('.reviews', {
-  // Optional parameters
+    const swiperNext = document.querySelector(".swiperNext");
+    const reviews = new Swiper(".reviews", {
+      // Optional parameters
 
-  loop: true,
-  slidesPerView: 3,
-   spaceBetween: 10,
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 10,
 
-  // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+      // If we need pagination
+      //   pagination: {
+      //     el: '.swiper-pagination',
+      //   },
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
+      //   // Navigation arrows
+      //   navigation: {
+      //     nextEl: '.swiper-button-next',
+      //     prevEl: '.swiper-button-prev',
+      //   },
 
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-}); 
-swiperPrev.addEventListener("click", () => {
-        reviews.slidePrev();
+      //   // And if we need scrollbar
+      //   scrollbar: {
+      //     el: '.swiper-scrollbar',
+      //   },
+    });
+    swiperPrev.addEventListener("click", () => {
+      reviews.slidePrev();
+    });
 
-     })
+    swiperNext.addEventListener("click", () => {
+      reviews.slideNext();
+    });
+  }
 
-     swiperNext.addEventListener("click", () => {
-        reviews.slideNext();
+  function addEventListener() {
+    const showReviewsBtn = document.querySelectorAll(".show-review");
+    const closeReviews = document.querySelectorAll(".close-reviews");
 
-     })
-}
+    showReviewsBtn.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        let element =
+          event.target.parentElement.parentElement.parentElement.parentElement
+            .nextElementSibling;
 
+        element.classList.add("show-reviews");
 
-function addEventListener() {
-    const showReviewsBtn = document.querySelectorAll(".show-review")
-    const closeReviews = document.querySelectorAll(".close-reviews")
+        // if(!showReviewsBtn) {
+      });
+    });
 
-    showReviewsBtn.forEach(item => {
-        item.addEventListener("click", (event) => {
-           let element = event.target.parentElement.parentElement.parentElement.parentElement.nextElementSibling 
+    closeReviews.forEach((item) => {
+      item.addEventListener("click", (event) => {
+        let element =
+          event.target.parentElement.parentElement.parentElement.parentElement;
+        console.log(element);
 
-           element.classList.add("show-reviews")
+        element.classList.remove("show-reviews");
 
-            // if(!showReviewsBtn) { 
-        })
-    })
+        // if(!showReviewsBtn) {
+      });
+    });
+  }
 
-    closeReviews.forEach(item => {
-        item.addEventListener("click", (event) => {
-           let element = event.target.parentElement.parentElement.parentElement.parentElement
-           console.log(element);
-           
-            
-           element.classList.remove("show-reviews")
-
-            // if(!showReviewsBtn) { 
-        })
-    })
-}
-
-
- function tabsProduct() {
+  function tabsProduct() {
     const tabsbtn = document.querySelectorAll(".tabs_nav");
     const tabsItem = document.querySelectorAll(".tabs_content");
 
@@ -134,24 +212,50 @@ function addEventListener() {
     });
   }
 
-  tabsProduct();
-addEventListener();
-swiper();
+  function anhorsLinks() {
+    const links = document.querySelectorAll('.mobile-menu a')
+    const modalBurger = document.querySelector(".burger-dialog")
+    console.log(anhorsLinks);
+    
+    links.forEach(link => {
+      link.addEventListener("click", function(e) {
+      
 
-changeAcardion();
+      modalBurger.classList.remove("active")
+       document.body.style.overflow = 'visible';
+    })
+    })
+    
+  }
+
+  anhorsLinks();
+
+ 
+
+
+
+
+
+// Закрытие по клавише Escape обеспечивается самим тегом <dialog>, 
+// но если вам нужно перехватить событие:
+
+
+  tabsProduct();
+  addEventListener();
+  swiper();
+  changeAcardionFaq();
+  changeAcardion();
 }
 
 init();
 
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const video = document.querySelector(".header-video-bg");
-  
+
   video.addEventListener("canplay", () => {
     video.classList.add("is-loaded");
   });
-  
+
   // Если видео уже закешировано
   if (video.readyState >= 3) {
     video.classList.add("is-loaded");
