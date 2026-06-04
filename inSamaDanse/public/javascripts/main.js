35199bc37268589f9525365373aecc35ede1c108
alexmysite <73168503+Aleksandr-Tsarrytsanskyi@users.noreply.github.com>
2026-06-04 12:24:02 +0300

    Update main.js

--- inSamaDanse/public/javascripts/main.js
+++ inSamaDanse/public/javascripts/main.js
@@ -346,6 +346,8 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("visibilitychange", function() {
     const video = document.querySelector(".header-video-bg");
     if (document.visibilityState === "visible" && video.paused) {
+      console.log("video");
+      
         video.play().catch(error => console.log("Автозапуск заблокирован:", error));
     }
 });
